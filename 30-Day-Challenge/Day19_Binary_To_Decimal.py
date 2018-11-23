754067d1f75c6b4cbfa0e0956591a44baf41516b
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-11-24 01:32:19 +0530

    Day 19
    Day 19 Solution
    Python
    TechGig 30 Day Challenge

---
+++ 30-Day-Challenge/Day19_Binary_To_Decimal.py
@@ -0,0 +1,13 @@
+def main():
+    
+    binary = int(input()) 
+    decimal, i, n = 0, 0, 0
+    while(binary != 0): 
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i) 
+        binary = binary//10
+        i += 1
+    print(decimal)
+
+main()
+
