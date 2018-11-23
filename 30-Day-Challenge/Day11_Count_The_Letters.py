47410943e8d6e54d8eb8fef432635208f4032bde
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-11-24 01:31:06 +0530

    Day 11
    Day 11 Solution
    Python
    TechGig 30 Day Challenge

---
+++ 30-Day-Challenge/Day11_Count_The_Letters.py
@@ -0,0 +1,13 @@
+def main():
+    string = input()
+    lower,upper = 0,0
+    
+    for i in range(len(string)):
+        if string[i].isupper() :
+            upper += 1
+        elif string[i].islower() :
+            lower += 1
+    print(upper)
+    print(lower)
+main()
+
