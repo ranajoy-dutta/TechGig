020a0434b2fb9340a29f4d22262b863b52da0888
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-11-24 01:08:19 +0530

    Day 7
    Day 7 Solution
    Python
    Techgig 30 Day challenge

---
+++ 30-Day-Challenge/Day7_Count_Special_Numbers_Between_Boundaries.py
@@ -0,0 +1,21 @@
+def main():
+    start = int(input())
+    stop = int(input())
+    count = 0
+    for i in range(start,stop):
+        j=2
+        flag = 0
+        while j<i:
+            if i%j == 0:
+                flag = 1
+                break
+            else:
+                flag=0
+                j+=1
+            
+        if flag == 0 and i!=1 and i!=2:
+            count += 1
+    print(count)
+            
+main()
+
