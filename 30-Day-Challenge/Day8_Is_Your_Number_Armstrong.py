036e51a0a9b63ec41014f522101198d259c4200e
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-11-24 01:08:30 +0530

    Day 8
    Day 8 Solution
    Python
    Techgig 30 Day challenge

---
+++ 30-Day-Challenge/Day8_Is_Your_Number_Armstrong.py
@@ -0,0 +1,20 @@
+def main():
+    num = int(input())
+    cpy = num
+    arr=[]
+    sum = 0
+    while int(num) > 0:
+        arr.append(int(num % 10))
+        num/=10
+    n = len(arr)
+    
+    for i in range(n):
+        sum += arr[i]**n
+        
+    if sum == cpy:
+        print('True')
+    else :
+        print('False')
+
+main()
+
