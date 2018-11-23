df4b3809d0adf2bfba75a7f5412cd0a9e72bbb79
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-11-24 01:07:14 +0530

    Day 4
    Day 4 Solution
    Techgig Solution
    Techgig 30 day challenge

---
+++ 30-Day-Challenge/Day4_Decide_Yourself_With_Conditional_Statement.py
@@ -0,0 +1,11 @@
+def main():
+    age = int(input())
+    if age<10:
+        print('I am happy as having no responsibilities.')
+    elif age<18:
+        print('I am still happy but starts feeling pressure of life.')
+    else:
+        print('I am very much happy as i handled the pressure very well.')
+    
+main()
+
