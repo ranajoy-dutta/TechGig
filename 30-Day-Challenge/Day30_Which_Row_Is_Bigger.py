7d479ac65d6ccf77198870b57de1e56e0b75d0e5
Ranajoy Dutta <ranajoydutta7@gmail.com>
2018-11-24 01:34:18 +0530

    Day 30
    Day 30 Solution
    Python
    TechGig 30 Day Challenge

---
+++ 30-Day-Challenge/Day30_Which_Row_Is_Bigger.py
@@ -0,0 +1,14 @@
+def main():
+    row, col = map(int,input().split())
+    mat = []
+    max=0
+    for i in range(row):
+        arr = list(map(int,input().split()))
+        if sum(arr)>max:
+            max = sum(arr)
+            rowid = i+1
+    print("Row",rowid)
+    
+    
+main()
+
