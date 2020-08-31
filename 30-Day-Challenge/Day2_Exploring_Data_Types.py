5886a2fd2daca0189452e6cd4a9255f5fc780a92
Ranajoy Dutta <ranajoydutta7@gmail.com>
2020-08-31 21:40:14 +0530

    Day 2

--- 30-Day-Challenge/Day2_Exploring_Data_Types.py
+++ 30-Day-Challenge/Day2_Exploring_Data_Types.py
@@ -1,18 +1,17 @@
 def main():
     a=input()
     if a.isalpha():
-        print("This input is of type string.")
+        print("This input is of type string.", end="")
     elif a.isnumeric():
-        print("This input is of type Integer.")
+        print("This input is of type Integer.", end="")
     elif a.rfind('.')!=-1:
         try:
             a=float(a)
-            print('This input is of type Float.')
+            print('This input is of type Float.', end="")
         except:
-            print("This input is of type string.")
+            print("This input is of type string.", end="")
     elif a[0]=='-':
-        print("This input is of type Integer.")
+        print("This input is of type Integer.", end="")
     else:
-        print('This is something else.')
+        print('This is something else.', end="")
 main()
-
