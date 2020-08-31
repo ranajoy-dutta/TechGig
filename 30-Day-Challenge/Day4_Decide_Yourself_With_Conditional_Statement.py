6a2e963fabfecbb8af8e595faa83d3f01a28e1cd
Ranajoy Dutta <ranajoydutta7@gmail.com>
2020-08-31 21:43:53 +0530

    Day 4

--- 30-Day-Challenge/Day4_Decide_Yourself_With_Conditional_Statement.py
+++ 30-Day-Challenge/Day4_Decide_Yourself_With_Conditional_Statement.py
@@ -1,11 +1,11 @@
 def main():
     age = int(input())
     if age<10:
-        print('I am happy as having no responsibilities.')
+        print('I am happy as having no responsibilities.', end="")
     elif age<18:
-        print('I am still happy but starts feeling pressure of life.')
+        print('I am still happy but starts feeling pressure of life.', end="")
     else:
-        print('I am very much happy as i handled the pressure very well.')
+        print('I am very much happy as i handled the pressure very well.', end="")
     
 main()
 
